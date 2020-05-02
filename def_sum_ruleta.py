6cc481503fd401662a0c9b6f40b2cce996ec7514
Darina Palko <p.melis@post.cz>
2020-05-02 19:34:43 +0200

    pridal sem def sucet ruleta

--- def_sum_ruleta.py
+++ def_sum_ruleta.py
@@ -1,15 +1,16 @@
 # Hlavicka funkce
-def ruleta (prvni,posledni):
-
+def ruleta(prvni, posledni):
     # Pomocna promenna
     vysledek = 0
-
     # For loop
-    for cislo in range(prvni,posledni):
-         vysledek += cislo
-
+    for cislo in range(prvni, posledni):
+        vysledek += cislo
     # Vraceni vysledku
     return vysledek
 
-vzpocet = ruleta(0,37)
-print (vzpocet)
+
+vzpocet = ruleta(0, 37)
+print(vzpocet)
+
+vzpocet = ruleta(5, 322)
+print(vzpocet)
