176adfcfeae8862ba32411fb75c6e0c639f42ff1
Darina Palko <p.melis@post.cz>
2020-05-02 19:31:52 +0200

    pridal jsem alert

---
+++ def_sum_ruleta.py
@@ -0,0 +1,15 @@
+# Hlavicka funkce
+def ruleta (prvni,posledni):
+
+    # Pomocna promenna
+    vysledek = 0
+
+    # For loop
+    for cislo in range(prvni,posledni):
+         vysledek += cislo
+
+    # Vraceni vysledku
+    return vysledek
+
+vzpocet = ruleta(0,37)
+print (vzpocet)
