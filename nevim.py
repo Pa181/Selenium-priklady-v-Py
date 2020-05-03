1813d46062e5f81c6ccd1a53b76c819453afd5b1
Darina Palko <p.melis@post.cz>
2020-05-03 11:32:12 +0200

    den,tyden,hod. v roce

--- nevim.py
+++ nevim.py
@@ -1,5 +1,17 @@
+def vanoce (x):
+    van = 358 - x
+    return van
 def den (x):
-    return rok = 365 - x
+    dni = 365 -x
+    tyden = dni / 7
+    ty =round(tyden)
+    return dni,ty
+def hodiny (x):
+    hod = (365-x) * 24
+    return hod
 x = int (input ('Kolikati den v roce je : '))
 y = den(x)
-print (y)
+print ("Do konce roku zbyva dni" ,y[0] ,"a tydnu " ,y[1] ,"A do konce roku zbyva tolik hodin",hodiny(x))
+print ("Do Vanoc zbyva dni" ,vanoce(x))
+
+print(round(-16.54))
